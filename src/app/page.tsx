763650a5c10b14c2f8f8fc992af8063c3e462762
shreejai <shreejai@icloud.com>
2023-08-26 23:23:00 +1000

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client"
+import { useState, FormEvent } from "react";
 export default function Home() {
+  const [inputVal, setInputVal] = useState("");
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault(); 
+    
+  }
   return (
     <div>
       <div>
         <h1>Enter your name</h1>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Type your name"/>
         <button type="submit"> Predict Data</button>
       </form>
