eb2b7626c10c7fc2b6f8df87ba3ae890c5b8d216
shreejai <shreejai@icloud.com>
2023-08-27 13:20:16 +1000

    useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-"use client"
+"use client";
+
 import { useState, FormEvent } from "react";
+import {useRouter} from 'next/navigation';
 export default function Home() {
   const [inputVal, setInputVal] = useState("");
-
+  const {push} = useRouter(); //  Destructure useRouter to use push function
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault(); 
-    
+    push(`/prediction/${inputVal}`);
   }
   return (
     <div>
@@ -13,7 +15,12 @@ export default function Home() {
         <h1>Enter your name</h1>
       </div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Type your name"/>
+        <input 
+          type="text" 
+          placeholder="Type your name"
+          value = {inputVal}
+          className="text-black"
+          onChange={(e) => setInputVal(e.target.value)}/>
         <button type="submit"> Predict Data</button>
       </form>
     </div>
